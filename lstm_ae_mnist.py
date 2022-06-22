75c56e9106435245fd35dc27fbfc93c2f26e19f7
Roy <roy15957@gmail.com>
2022-06-22 17:35:06 +0300

    first branch, gpu is working fine

--- lstm_ae_mnist.py
+++ lstm_ae_mnist.py
@@ -7,18 +7,23 @@ import koren2_ae
 import matplotlib.pyplot as plt
 import numpy as np
 
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
-def imshow(img):
+def imshow(img,some_string):
+    img = img.cpu()
     img = img / 2 + 0.5
     npimg = img.numpy()
+    plt.title(some_string)
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
     pass
 
 # hyper-params
-epoch_num = 3
-batch_size = 20
-
+epoch_num = 200
+batch_size = 100
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print("Using the device: ",device)
 
 transform = transforms.Compose(
     [transforms.ToTensor(),
@@ -29,39 +34,38 @@ trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuff
 
 testset = torchvision.datasets.MNIST(root='./data', train=False,
                                        download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+testloader = torch.utils.data.DataLoader(testset, batch_size=3,
                                          shuffle=False)
 
-model = koren2_ae.koren_AE(28, 24)
+model = koren2_ae.koren_AE(28, 15)
 model = model.double()
-opt = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-# opt = optim.Adam(model.parameters(), lr=1e-3)
+# opt = optim.SGD(model.parameters(), lr=0.1, momentum=0.9)
+opt = optim.Adam(model.parameters(), lr=0.001)
 criterion = nn.MSELoss()
 
 
-def train():
-    for epoch in range(epoch_num):
-        total_loss = 0.0
-        # iterate over the dataset
-        for i, data in enumerate(trainloader):
-            inputs, labels = data
-            inputs = torch.squeeze(inputs)
-            # inputs = torch.squeeze(inputs, 1)
-            # inputs = torch.reshape(inputs, (inputs.shape[0], 28 * 28))
-            inputs = inputs.double()
-            opt.zero_grad()
-            outputs = model(inputs)
-            loss = criterion(outputs, inputs)
-            loss.backward()
-            nn.utils.clip_grad_value_(model.parameters(), clip_value=1.0)  # gradient clipping
-            opt.step()
-            # print stats
-            total_loss += loss.item()
-            if i % 100 == 99:
-                    print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, total_loss/100))
-            total_loss = 0
+for epoch in range(epoch_num):
+    total_loss = 0.0
+    # iterate over the dataset
+    for i, data in enumerate(trainloader):
+        inputs, labels = data
+        inputs = torch.squeeze(inputs)
+        # inputs = torch.squeeze(inputs, 1)
+        # inputs = torch.reshape(inputs, (inputs.shape[0], 28 * 28))
+        inputs = inputs.double()
+        opt.zero_grad()
+        inputs = inputs.to(device)
+        model = model.to(device)
+        outputs = model(inputs)
+        loss = criterion(outputs, inputs)
+        loss.backward()
+        nn.utils.clip_grad_value_(model.parameters(), clip_value=1.0)  # gradient clipping
+        opt.step()
+        # print stats
+        total_loss += loss.item()
+    print('[%d] loss: %.7f' % (epoch + 1, total_loss))
+    total_loss = 0
 
-train()
 
 with torch.no_grad():
     total, correct = 0, 0
@@ -69,10 +73,11 @@ with torch.no_grad():
         inputs, _ = data
         inputs = torch.squeeze(inputs)
         inputs = inputs.double()
+        inputs = inputs.to(device)
         # inputs = torch.reshape(inputs, (inputs.shape[0], 28 * 28))
         outputs = model(inputs)
-        imshow(torchvision.utils.make_grid(torch.unsqueeze(inputs, 1)))
-        imshow(torchvision.utils.make_grid(torch.unsqueeze(outputs, 1)))
+        imshow(torchvision.utils.make_grid(torch.unsqueeze(inputs, 1)),"Original")
+        imshow(torchvision.utils.make_grid(torch.unsqueeze(outputs, 1)),"Reconstructed")
         exit()
 
 
