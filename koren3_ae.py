b0243f7e202f6dd9c5771fecde3307a4111c9c6d
Roy <roy15957@gmail.com>
2022-06-29 18:01:54 +0300

    BO NELEH SHUV LBARAKA

---
+++ koren3_ae.py
@@ -0,0 +1,134 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+#
+class Encoder(nn.Module):
+    def __init__(self, input_dim, out_dim, h_activ, out_activ):
+        super(Encoder, self).__init__()
+        self.num_layers = input_dim
+        self.layers = nn.ModuleList()
+        # for i in range(input_dim-out_dim):  # decreasing the dimension in 1 in each lstm til output dim
+        #     layer = nn.LSTM(
+        #         input_size=input_dim-i,
+        #         hidden_size=input_dim-i-1,
+        #         num_layers=1,
+        #         batch_first=True
+        #     )
+        #     self.layers.append(layer)
+        # for i in range(out_dim):
+        #     layer = nn.LSTM(
+        #         input_size=out_dim,
+        #         hidden_size=out_dim,
+        #         num_layers=1,
+        #         batch_first=True
+        #     )
+        #     self.layers.append(layer)
+        layer = nn.LSTM(
+            input_size=input_dim,
+            hidden_size=out_dim,
+            num_layers=1,
+            batch_first=True
+        )
+        self.layers.append(layer)
+
+        self.h_activ, self.out_activ = h_activ, out_activ
+
+    def forward(self, x):
+        # x = x.unsqueeze(0)
+        # exit()
+        for index, layer in enumerate(self.layers):
+            x, (h_n, c_n) = layer(x)
+            if self.h_activ and index < self.num_layers - 1:
+                x = self.h_activ(x)
+            elif self.out_activ and index == self.num_layers - 1:
+                return self.out_activ(x).squeeze()
+        return x.squeeze()
+
+
+class Decoder(nn.Module):
+    def __init__(self, input_dim, out_dim, h_activ):
+        super(Decoder, self).__init__()
+        self.num_layers = input_dim
+        self.layers = nn.ModuleList()
+        # for i in range(out_dim-input_dim):
+        #     layer = nn.LSTM(
+        #         input_size=input_dim+i,
+        #         hidden_size=input_dim+i+1,
+        #         num_layers=1,
+        #         batch_first=True
+        #     )
+        #     self.layers.append(layer)
+        # for i in range(input_dim):
+        #     layer = nn.LSTM(
+        #         input_size=out_dim,
+        #         hidden_size=out_dim,
+        #         num_layers=1,
+        #         batch_first=True
+        #     )
+        #     self.layers.append(layer)
+        layer = nn.LSTM(
+            input_size=input_dim,
+            hidden_size=out_dim+1,
+            num_layers=1,
+            batch_first=True
+        )
+        self.layers.append(layer)
+
+        self.h_activ = h_activ
+        # self.dense_matrix = nn.Parameter(
+        #     torch.rand((out_dim, out_dim), dtype=torch.float),
+        #     requires_grad=True
+        # )
+
+    def forward(self, x, seq_len):
+        x, (h_n, c_n) = self.layers[0](x)  # only one block (of N layers)
+        # for index, layer in enumerate(self.layers):
+        #     x, (h_n, c_n) = layer(x)
+        #
+        #     if self.h_activ and index < self.num_layers - 1:
+        #         x = self.h_activ(x)
+        return x
+
+
+
+class koren_AE(nn.Module):
+    def __init__(self, input_dim, encoding_dim, classification=False, prediction=True, pix_by_pix = False, h_activ=nn.Sigmoid(),
+                 out_activ=nn.Tanh()):
+        super(koren_AE, self).__init__()
+        self.encoding_dim = encoding_dim
+        self.encoder = Encoder(input_dim, encoding_dim, h_activ, out_activ)
+        self.decoder = Decoder(encoding_dim, input_dim, h_activ)
+        self.conv1 = nn.Conv2d(1, 6, 5)   # maybe 5X5
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 4 * 4, 120)  # 4*4 are the remaining pixels. was 28X28 -> 24X24 -> 12X12 -> 8X8 -> 4X4
+        self.fc2 = nn.Linear(120, 84)          # because according to the picture up, after every conv there is max-pooling
+        self.fc3 = nn.Linear(84, 10)
+        self.classify = classification
+        self.pix_by_pix = pix_by_pix
+        # self.linearDecoder = torch.nn.Linear(60, 1)
+        self.prediction = prediction
+
+    def forward(self, x, classification):
+        seq_len = x.shape[0]
+        x = self.encoder(x)
+        if self.pix_by_pix == True:
+            x = torch.reshape(x, (x.shape[0], 1, self.encoding_dim))
+        x = self.decoder(x, seq_len)
+        clone_x = x
+        if classification:
+            x = torch.reshape(x, (x.shape[0], 28, 28))
+
+        if self.classify == True:
+            x = x.unsqueeze(1)
+            x = self.pool(F.relu(self.conv1(x)))
+            x = self.pool(F.relu(self.conv2(x)))
+            x = torch.flatten(x, 1) # flatten all dimensions except batch
+            x = F.relu(self.fc1(x))
+            x = F.relu(self.fc2(x))
+            x = self.fc3(x)
+            return clone_x, x
+        # x = self.linearDecoder(x)
+        return x
+
--- lstm_ae_snp500.py
+++ lstm_ae_snp500.py
@@ -1,17 +1,25 @@
 import numpy as np
 import pandas as pd
-import warnings; warnings.simplefilter('ignore')
+import warnings
+
+warnings.simplefilter('ignore')
 import matplotlib.pyplot as plt
-import seaborn as sns; sns.set()
+import seaborn as sns
+import torchvision.transforms as transforms
+
+# sns.set()
 import torch
 import torch.nn as nn
+import koren3_ae
 import koren2_ae
 import torch.optim as optim
 from snp500dataset import sp500Dataset
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
 
 def plot_points(g_t, prediction):
-    x = [i+1 for i in range(1007)]
+    x = [i for i in range(1007)]
     prediction = prediction.cpu()
     g_t = g_t.cpu()
     plt.plot(x, g_t, label='original stock')
@@ -21,12 +29,48 @@ def plot_points(g_t, prediction):
     plt.xlabel('day')
     plt.show()
 
+
+def plot_points2(g_t, prediction):
+    x = [i for i in range(19)]
+    prediction = prediction.cpu()[-19:]
+    g_t = g_t.cpu()[-19:]
+    plt.plot(x, g_t, label='original stock')
+    plt.plot(x, prediction, label='reconstruction stock')
+    plt.legend()
+    plt.ylabel('value')
+    ax = plt.gca()
+    ax.set_ylim([0, 2])
+    plt.xlabel('day')
+    plt.show()
+
+
+def plot_points3(g_t, prediction):
+    x = [i for i in range(27)]
+    plt.plot(x, g_t, label='true')
+    plt.plot(x, prediction, label='prediction')
+    plt.legend()
+    plt.ylabel('value')
+    plt.xlabel('day')
+    plt.show()
+
+
+def plot_losses(train_loss, pred_loss):
+    x = [i for i in range(len(train_loss))]
+    plt.plot(x, train_loss, label='Train Loss')
+    plt.plot(x, pred_loss, label='Prediction Loss')
+    plt.legend()
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.show()
+
+
 stocks = pd.read_csv('SP500.csv', error_bad_lines=False)
+
 
 def plotStocks():
     # plot daily max values for google and amazon during the years
     stocks = pd.read_csv('SP500.csv', error_bad_lines=False)
-    fig, ax = plt.subplots(2, sharex=True, figsize=(16,6))
+    fig, ax = plt.subplots(2, sharex=True, figsize=(16, 6))
     amazon = stocks.loc[stocks['symbol'] == 'AMZN']
     google = stocks.loc[stocks['symbol'] == 'GOOGL']
     amazon.groupby('date')['high'].sum().plot(ax=ax[0])
@@ -36,40 +80,58 @@ def plotStocks():
     plt.show()
 
 
-epoch_num = 500
+epoch_num = 2
 batch_size = 20
 time_size = 1007
 input_size = 1
-seq_len = 53  # split the 1007 days into 19 * 53
-latent_dim = 30
+t = 26
 prediction = True
+question3dot3 = True
 data_gen = sp500Dataset()
 trainloader, validation_data = data_gen.getDataForModel(batch_size, time_size)
 validationloader = validation_data.reshape(len(validation_data), input_size, time_size)
 
-
-
 # hyper-params
-
+begin = 0
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print("Using the device: ",device)
 
+if prediction == False:
+    seq_len = 53
+    latent_dim = 30
+    model = koren2_ae.koren_AE(seq_len, latent_dim, classification=False)
+elif prediction == True and question3dot3 == True:
+    seq_len = 52
+    latent_dim = 30
+    model = koren3_ae.koren_AE(seq_len, latent_dim, classification=False)
+else:  # gimel
+    t = 26
+    seq_len = t
+    latent_dim = 30
+    model = koren3_ae.koren_AE(seq_len, latent_dim, classification=False)
 
-
-model = koren2_ae.koren_AE(seq_len, latent_dim, classification=False)
 model = model.double()
 opt = optim.Adam(model.parameters(), lr=0.01)
 criterion = nn.MSELoss()
+training_loss = []
+prediction_loss = []
 
 
-def prediction_criterion(criterion1, criterion2, outputs, inputs, labels_out, labels):
+def prediction_criterion(criterion1, outputs, inputs, pred, gt_pred):
     lam1, lam2 = 1, 1
-    return lam1 * criterion1(outputs, inputs) + lam2 * criterion2(labels_out, labels)
+    train_loss = criterion1(outputs, inputs)
+    pred_loss = criterion1(pred, gt_pred)
+
+    return lam1 * criterion1(outputs, inputs) + lam2 * criterion1(pred, gt_pred), train_loss, pred_loss
 
 
 # iterate over #epochs
 for epoch in range(epoch_num):
     total_loss = 0.0
+    train_toal_loss =0.0
+    pred_toal_loss =0.0
+    pred_loss = None
+    train_loss =None
     # iterate over the dataset
     for i, data in enumerate(trainloader):
         inputs = data
@@ -81,22 +143,52 @@ for epoch in range(epoch_num):
         # inputs = inputs.unsqueeze(0)
         inputs = inputs.to(device)
         model.to(device)
-        if prediction == True:
-            target = inputs[:, :, 1:53]
-            inputs = inputs[:, : ,:52]
-            outputs, label_out = model(inputs, classification=False, prediction=True) # we need to make the AE support prediction
-            loss = prediction_criterion(criterion, criterion, outputs, inputs, label_out, target)
-        else:
+        loss = None
+        if prediction == False:
+            target = inputs[:, :, :53]
+            inputs = inputs[:, :, :52]
+            outputs, label_out = model(inputs, classification=False)  # we need to make the AE support prediction
+            loss = prediction_criterion(criterion, outputs, inputs, label_out, target)
+            loss.backward()
+        elif question3dot3 == True:
+            target = inputs[:, :, 52]
+            inputs = inputs[:, :, :52]
             outputs = model(inputs, False)
-            loss = criterion(outputs, inputs)
-        loss.backward()
-        nn.utils.clip_grad_value_(model.parameters(), clip_value=1.0) # gradient clipping
+            loss, train_loss, pred_loss = prediction_criterion(criterion, outputs[:, :, :52], inputs, outputs[:, :, 52],
+                                                               target)
+            loss.backward()
+
+        else:
+            original = inputs
+            while t != 52:
+
+                target = original[:, :, t]  # 27 bindex 26
+                inputs = original[:, :, begin:t]  # 26 items mindex 0 ad 25
+
+                outputs = model(inputs, False)
+                loss, train_loss, pred_loss = prediction_criterion(criterion, outputs[:, :, :26], inputs,
+                                                                   outputs[:, :, 26],
+                                                                   target)
+                t += 1
+                begin += 1
+                loss.backward()
+            t = 26
+            begin = 0
+
+        nn.utils.clip_grad_value_(model.parameters(), clip_value=1.0)  # gradient clipping
         opt.step()
         # print stats
         total_loss += loss.item()
+        train_toal_loss += train_loss
+        pred_toal_loss += pred_loss
+
+    training_loss.append(train_loss.cpu().detach().numpy())
+    prediction_loss.append(pred_loss.cpu().detach().numpy())
+
     print('[%d] loss: %.6f' % (epoch + 1, total_loss))
 
 with torch.no_grad():
+
     total, correct = 0, 0
     counter = 0
     for data in validationloader:
@@ -104,15 +196,18 @@ with torch.no_grad():
         inputs = torch.squeeze(inputs)
         inputs = inputs.double()
         inputs = inputs.to(device)
+        print(inputs.shape)
         inputs = torch.reshape(inputs, (1, 19, 53))
-
-        outputs = model(inputs, False)
+        print(inputs[:, :, :52].shape)
+        outputs = model(inputs[:, :, :52], False)
         outputs = torch.reshape(outputs, (1, 1, 1007))
         inputs = torch.reshape(inputs, (1, 1, 1007))
         outputs = outputs.squeeze()
         inputs = inputs.squeeze()
         plot_points(inputs, outputs)
-        counter += 1
-        if counter == 2:
-            break
+        plot_points2(inputs, outputs)
 
+        counter += 1
+        if counter == 3:
+            break
+    plot_losses(training_loss, prediction_loss)
